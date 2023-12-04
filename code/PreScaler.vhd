bb5396d9c092fe12f2d4ba87f29b2b91a19769dd
SshuLuo <152819969+SshuLuo@users.noreply.github.com>
2023-12-03 17:12:59 -0800

    Update PreScaler.vhd

--- code/PreScaler.vhd
+++ code/PreScaler.vhd
@@ -6,7 +6,7 @@ Entity PreScaler IS
 port(
 setSpeed : in std_logic_vector(2 downto 0);
 clkI : in std_logic;
-signal clkO : out std_logic
+clkO : out std_logic
 );
 END PreSCalEr;
 
@@ -17,25 +17,36 @@ eIn : in std_logic_vector(2 downto 0);
 eout : out unsigned(1 downto 0)
 );
 end component;
-signal fTemp : unsigned(2499999 downto 0):= (others =>'0');
-signal nTemp : unsigned(4999999 downto 0):= (others =>'0');
-signal sTemp : unsigned(9999999 downto 0):= (others =>'0');
+signal fTemp : unsigned(23 downto 0):= (others =>'0');
+signal nTemp : unsigned(23 downto 0):= (others =>'0');
+signal sTemp : unsigned(23 downto 0):= (others =>'0');
 signal speed : unsigned(1 downto 0):= (others =>'0');
 Begin
 instance1 : encoder32 port map (setSpeed, speed);
 process(clkI, speed)
 Begin
-iF rising_edge(clkI) and speed = 1 Then
+iF rising_edge(clkI) and ntemp = "100110001001011010000000" AND speed = 1 Then
+stemp<=(others=>'0');
+clkO <= std_logic(stemp(23));
+elsif rising_edge(clkI) and speed = 1 Then
 stemp <= stemp + 1;
-clkO <= std_logic(stemp(9999999));
+clkO <= std_logic(stemp(23));
 End if;
-iF rising_edge(clkI) and speed = 2 Then
-ntemp <= ntemp + 1;
-clkO <= std_logic(stemp(4999999));
+
+iF rising_edge(clkI) and ntemp = "010011000100101101000000" AND speed = 2 Then
+stemp<=(others=>'0');
+clkO <= std_logic(stemp(23));
+elsif rising_edge(clkI) and speed = 1 Then
+stemp <= stemp + 1;
+clkO <= std_logic(stemp(23));
 End if;
-iF rising_edge(clkI) and speed = 3 Then
-ftemp <= ftemp + 1;
-clkO <= std_logic(stemp(2499999));
+
+iF rising_edge(clkI) and ntemp = "001001100010010110100000" AND speed = 3 Then
+stemp<=(others=>'0');
+clkO <= std_logic(stemp(23));
+elsif rising_edge(clkI) and speed = 1 Then
+stemp <= stemp + 1;
+clkO <= std_logic(stemp(23));
 End if;
 end process;
 end behavior;
